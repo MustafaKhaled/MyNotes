3e1385514b40fa8aa531b6dd9e7b9b70ba3f8ace
Mustafa.Khaled <mustafa.khaled@intercom.com.eg>
2019-05-10 00:19:06 +0200

    Adding bottom sheet, configure database and home page components

---
+++ app/src/main/java/com/mynotes/project/features/home/ui/reyclerview/NoteAdapter.java
@@ -0,0 +1,24 @@
+package com.mynotes.project.features.home.ui.reyclerview;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteViewHolder> {
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
